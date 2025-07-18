a4ff1a7c869f610a5e37025a00334e8b2e5baccb
dvdvdvbudvbv1 <dvdvdvbudvbv@gmail.com>
2025-07-18 23:41:07 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function openBox() {
+  document.querySelector('.lid').style.transform = 'rotateX(-130deg)';
+  document.querySelector('.ring').style.opacity = '1';
+  document.getElementById('proposalText').style.opacity = '1';
+}
