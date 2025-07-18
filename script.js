96ef09ff0e660e8365e4d7acc552221a57ca04e1
dvdvdvbudvbv1 <dvdvdvbudvbv@gmail.com>
2025-07-18 23:47:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function openBox() {
-  document.querySelector('.lid').style.transform = 'rotateX(-130deg)';
+  document.querySelector('.lid').style.transform = 'rotateX(-120deg)';
   document.querySelector('.ring').style.opacity = '1';
   document.getElementById('proposalText').style.opacity = '1';
+  document.getElementById('yesBtn').style.opacity = '1';
+}
+
+function yesClicked() {
+  const btn = document.getElementById('yesBtn');
+  btn.textContent = '❤️ Спасибо! 💖';
+  btn.classList.add('clicked');
 }
